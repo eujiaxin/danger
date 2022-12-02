dd71429fd9c27efec3e47f4a34a90c4f37e81370
heheheejin <e_jaxx@hotmail.com>
2022-12-03 06:21:52 +0800

    Create homepage with password input

---
+++ src/pages/Home.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import styles from "./Home.module.css";
+
+const Home = () => {
+    const [enteredPassword, setEnteredPassword] = useState("");
+
+    const passwordChangeHandler = (e) => {
+        setEnteredPassword(e.target.value);
+    };
+
+    const formSubmitHandler = (e) => {
+        e.preventDefault(); // prevents page from reloading
+
+        // TODO: form validation - empty input
+        // redirectToPage(password); // seems dangerous to do this???
+    };
+
+    return (
+        <>
+            <input
+                type="password"
+                className={`${styles.input} ${styles.centered}`}
+                onChange={passwordChangeHandler}
+            />
+            <span className={styles.centered2}>{enteredPassword}</span>
+        </>
+    );
+};
+
+export default Home;
